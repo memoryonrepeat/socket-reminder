a6177a9f32b960863cc22e3a0803f9fc316039ad
Thanh Nguyen <tienthanh8490@gmail.com>
2020-09-07 00:56:39 +0200

    basic socket logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const redis = require('redis')
+/* const redis = require('redis')
 const client = redis.createClient({host: 'redis'})
 
 client.on('ready', function (error) {
@@ -10,3 +10,31 @@ client.on('error', function (error) {
 })
 
 console.log('server started!.....')
+*/
+const handler = (req, res) => {
+  fs.readFile(
+    __dirname + '/index.html',
+    (err, data) => {
+      if (err) {
+        res.writeHead(500)
+        return res.end('Error loading index.html')
+      }
+
+      res.writeHead(200)
+      res.end(data)
+    }
+  )
+}
+const app = require('http').createServer(handler)
+const io = require('socket.io')(app)
+const fs = require('fs')
+
+app.listen(8081)
+
+io.on('connection', (socket) => {
+  socket.emit('welcome', {hello: 'world'})
+
+  socket.on('newremind', (data) => {
+    socket.emit('remind', {name: data.name, time: data.time})
+  })
+})
